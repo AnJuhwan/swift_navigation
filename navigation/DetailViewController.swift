1a9077de162a3b04de8c13ff973183058242a6bf
AnJuHwan <juhwan@thetrive.com>
2023-02-19 11:52:58 +0900

    navigation

---
+++ navigation/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  navigation
+//
+//  Created by 안주환 on 2023/02/04.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#fileID, #function, #line, "- DetailViewController")
+    }
+}
--- navigation/ViewController.swift
+++ navigation/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var navToSecondVCBtn: UIButton!
+    @IBOutlet var navToDetailVCBtn: UIButton!
+    
+    var stepNumber: Int = 1 {
+        didSet {
+            self.title = "스탭넘버: \(stepNumber)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navToSecondVCBtn.addTarget(self, action: #selector(navToSecondVC), for: .touchUpInside)
+        navToDetailVCBtn.addTarget(self, action: #selector(navToDetailVC), for: .touchUpInside)
     }
 
-
+    @objc fileprivate func navToDetailVC(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "navToDetailVC", sender: self)
+        print(#fileID, #function, #line, "- detail")
+    }
+    
+    @objc fileprivate func navToSecondVC(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "navToSecondVC", sender: self)
+        print(#fileID, #function, #line, "- second")
+    }
+    
+    @IBAction func onPushBtnClicked(_ sender: UIButton) {
+        
+        // storyboard
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let viewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        
+        viewController.stepNumber = stepNumber + 1
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
 }
 
